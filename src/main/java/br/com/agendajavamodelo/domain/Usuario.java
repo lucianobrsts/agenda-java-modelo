58b3ac6f1e8db3224bcd226ff79a340c885de029
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-09-07 16:03:17 -0300

    Finalizada entidade Usuario

--- src/main/java/br/com/agendajavamodelo/domain/Usuario.java
+++ src/main/java/br/com/agendajavamodelo/domain/Usuario.java
@@ -5,5 +5,43 @@ public class Usuario {
 	private String nome;
 	private String login;
 	private String senha;
-	
+
+	public Usuario() {
+	}
+
+	public Usuario(String nome, String login, String senha) {
+		this.nome = nome;
+		this.login = login;
+		this.senha = senha;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [nome=" + nome + ", login=" + login + ", senha=" + senha + "]";
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
 }
