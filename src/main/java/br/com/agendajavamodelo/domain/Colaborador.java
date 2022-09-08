763177a13c7c246e49d52ff01827450a130dd4c1
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-09-08 20:50:58 -0300

    Adicionado entidade Colaborador

---
+++ src/main/java/br/com/agendajavamodelo/domain/Colaborador.java
@@ -0,0 +1,49 @@
+package br.com.agendajavamodelo.domain;
+
+public class Colaborador {
+
+	private String nome;
+	private String observacao;
+	private Servico servico;
+
+	public Colaborador() {
+
+	}
+
+	public Colaborador(String nome, String observacao, Servico servico) {
+		super();
+		this.nome = nome;
+		this.observacao = observacao;
+		this.servico = servico;
+	}
+
+	@Override
+	public String toString() {
+		return "Colaborador [nome=" + nome + ", observacao=" + observacao + ", servico=" + servico + "]";
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+
+	public Servico getServico() {
+		return servico;
+	}
+
+	public void setServico(Servico servico) {
+		this.servico = servico;
+	}
+
+}
