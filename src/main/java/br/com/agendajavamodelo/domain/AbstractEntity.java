33bb5b9d4c017bd0f625f4eb05e55c9fc226b75e
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-09-08 21:00:04 -0300

    Adicionado entidade AbstractEntity

---
+++ src/main/java/br/com/agendajavamodelo/domain/AbstractEntity.java
@@ -0,0 +1,11 @@
+package br.com.agendajavamodelo.domain;
+
+import java.io.Serializable;
+
+public class AbstractEntity implements Serializable {
+
+	private static final long serialVersionUID = -6302920971743000821L;
+	
+	protected Long id;
+
+}
