863d89babdc7b724687395496389782bea2ec3bc
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-09-15 19:08:52 -0300

    Mapeada abstractEntity para gerar id automaticamente

--- src/main/java/br/com/agendajavamodelo/domain/AbstractEntity.java
+++ src/main/java/br/com/agendajavamodelo/domain/AbstractEntity.java
@@ -2,13 +2,21 @@ package br.com.agendajavamodelo.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 import lombok.Data;
 
 @Data
+@MappedSuperclass
 public class AbstractEntity implements Serializable {
 
 	private static final long serialVersionUID = -6302920971743000821L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Long id;
 
 }
