4ad2f95ee4b9abfe5b7fa8403cf6bdbf3dd9c9da
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-09-21 22:30:39 -0300

    Finalizada refatoração da entidade cliente

--- src/main/java/br/com/agendajavamodelo/domain/Cliente.java
+++ src/main/java/br/com/agendajavamodelo/domain/Cliente.java
@@ -2,31 +2,43 @@ package br.com.agendajavamodelo.domain;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper=false)
-public class Cliente extends AbstractEntity {
+public class Cliente extends AbstractEntity{
+	
+	private static final long serialVersionUID = -2236845954696800900L;
 
-	private static final long serialVersionUID = 268271021148204370L;
-
+	@Column(name = "nome", nullable = false, length = 60)
 	private String nome;
-	private Date dataNascimento;
-	private String Celular;
-	private String whatsApp;
+	
+	@Column(name = "celular", nullable = false, length = 20)
+	private String celular;
+	
+	@Column(name = "whatsapp", length = 20)
+	private String whatsapp;
+	
+	@Column(name = "observacao", length = 80)
 	private String observacao;
+	
+	@Column(name = "data_nascimento")
+	private Date dataNascimento;
 
 	public Cliente() {
-
+		
 	}
 
-	public Cliente(String nome, Date dataNascimento, String celular, String whatsApp, String observacao) {
+	public Cliente(String nome, String celular, String whatsapp, String observacao, Date dataNascimento) {
+		super();
 		this.nome = nome;
-		this.dataNascimento = dataNascimento;
-		Celular = celular;
-		this.whatsApp = whatsApp;
+		this.celular = celular;
+		this.whatsapp = whatsapp;
 		this.observacao = observacao;
+		this.dataNascimento = dataNascimento;
 	}
 
 }
