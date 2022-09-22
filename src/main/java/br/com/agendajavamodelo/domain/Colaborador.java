eb2e3801f35992fcf79b85291639ad315887ecd7
lucianobrsts <lucianobrsts@yahoo.com.br>
2022-09-21 22:32:13 -0300

    Finalizada refatoração da entidade colaborador

--- src/main/java/br/com/agendajavamodelo/domain/Colaborador.java
+++ src/main/java/br/com/agendajavamodelo/domain/Colaborador.java
@@ -1,26 +1,29 @@
 package br.com.agendajavamodelo.domain;
 
+import javax.persistence.Column;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper=false)
-public class Colaborador extends AbstractEntity {
+public class Colaborador extends AbstractEntity{
 
-	private static final long serialVersionUID = -994877661371176228L;
+	private static final long serialVersionUID = -5873555166532282377L;
 
+	@Column(name = "nome", nullable = false, length = 60)
 	private String nome;
+	
+	@Column(name = "observacao", length = 100)
 	private String observacao;
-	private Servico servico;
 
 	public Colaborador() {
-
+		
 	}
 
-	public Colaborador(String nome, String observacao, Servico servico) {
+	public Colaborador(String nome, String observacao) {
 		this.nome = nome;
 		this.observacao = observacao;
-		this.servico = servico;
 	}
 
 }
